e1269fadae7fa133acffb6aaced1276f1539821b
rencan97 <work.rencan@gmail.com>
2023-02-25 11:16:39 -0300

    polygon v1

--- polygon.py
+++ polygon.py
@@ -1,36 +1,54 @@
 from unittest import main
 
 class Rectangle:
-    def __init__(self, height, width = 0):
+    def __init__(self, width, height):
         self.height = height
-        if width == 0:
-            width = height
-        self.width = height
+        self.width = width
+    def __str__(self):
+        string = type(self).__name__
+        valueVariableString = "(width={}, height={})".format(self.width, self.height)
+        return (type(self).__name__) + valueVariableString
+
+    def set_height(self, height):
+        self.height = height
+    def set_width(self, width):
+        self.width = width
+
 
     def get_area(self):
         return self.width * self.height
-    def set_width(self, width):
-        self.width = width
     def get_perimeter(self):
         return (self.width + self.height)*2
     def get_diagonal(self):
         return (self.width ** 2 + self.height ** 2) ** .5
+    def get_picture(self):
+        picture = ""
+        if(self.height > 50 or self.width > 50):
+            return "Too big for picture."
+        else:
+            for heightLine in range(self.height):
+                for widthColumn in range(self.width):
+                    picture+= "*"
+                picture += "\n"
+            return picture
+    def get_amount_inside(self, anotherShape):
+        fitHeight = self.height/anotherShape.height
+        fitWidth = self.width/anotherShape.width
+        return int(fitHeight * fitWidth)
+
 
         
 
 class Square(Rectangle):
-    def set_side(self, value):
-        self.width = value
-        self.height = value
+    def __init__ (self, side):
+        self.width = side
+        self.height = side
 
-rect = Rectangle(5, 10)
-print(rect.get_area())
-rect.set_width(3)
-print(rect.get_perimeter())
-print(rect)
+    def __str__(self):
+        return (type(self).__name__+"(side=" + str(self.width) + ")")
 
-sq = Square(9)
-print(sq.get_area())
-sq.set_side(4)
-print(sq.get_diagonal())
-print(sq)
+    def set_side(self, side):
+        self.width = side
+        self.height = side
+
+rect = Rectangle(3, 6)
